0ef61e2aaed88d9c45009343fd975cb0dbff8ec9
whatyouneedq <45916429+whatyouneedq@users.noreply.github.com>
2025-03-24 02:04:28 +0700

    da
    Added Items

--- app.py
+++ app.py
@@ -2,17 +2,17 @@ from flask import Flask, render_template, request, redirect, url_for
 import random
 from linecache import getline
 import os
+import json
 
 app = Flask(__name__)
-with open('id_to_tag.txt', 'r', encoding='utf-8') as f:
-        choicesnum = sum(1 for line in f)
+choicesnum = 976
 
 def load_choices():
     id1, id2 = random.sample(range(choicesnum), 2)
     tag1, tag2 = getline("id_to_tag.txt", id1+1).split(" - ")[1][:-1], getline("id_to_tag.txt", id2+1).split(" - ")[1][:-1]
     choice1, choice2 = [], []
     script_dir = os.path.dirname(__file__)
-    with open(os.path.join(script_dir, f"database/items/{tag1}.txt"), 'r', encoding='ANSI') as f:
+    with open(os.path.join(script_dir, f"database/items/{tag1}.txt"), 'r', encoding='windows-1251') as f:
         f.readline()
         choice1.append(f.readline().split(": ")[1][:-1])
         choice1.append(f"/static/images/{tag1}.jpg")
@@ -21,8 +21,9 @@ def load_choices():
             choice1.append(x)
         else:
             choice1.append(x[:99] + '...')
+        choice1.append(id1)
 
-    with open(os.path.join(script_dir, f"database/items/{tag2}.txt"), 'r', encoding='ANSI') as f:
+    with open(os.path.join(script_dir, f"database/items/{tag2}.txt"), 'r', encoding='windows-1251') as f:
         f.readline()
         choice2.append(f.readline().split(": ")[1][:-1])
         choice2.append(f"/static/images/{tag2}.jpg")
@@ -31,12 +32,19 @@ def load_choices():
             choice2.append(x)
         else:
             choice2.append(x[:99] + '...')
-    return choice1, choice2    #[name, imagepath, info]
+        choice2.append(id2)
+    return choice1, choice2    #[name, imagepath, info, id]
 
-# Запись результатов в файл
-def log_result(choice):
-    with open('results.txt', 'a', encoding='utf-8') as f:
-        f.write(f"{choice}\n")
+def log_choice(win, lose):
+    with open('results.json', 'r') as f:
+        players = json.load(f)
+
+    if win in players and lose in players:
+        players[win]['w'] += 1
+        players[lose]['l'] += 1
+
+    with open('results.json', 'w') as f:
+        json.dump(players, f)
 
 @app.route('/')
 def index():
@@ -45,12 +53,11 @@ def index():
 
 @app.route('/choose', methods=['POST'])
 def choose():
-    selected_choice = request.form['choice']
+    win = request.form['choice+']
+    lose = request.form['choice-']
     count = int(request.form['count']) + 1
 
-    # Запись результата в файл
-    with open('result.txt', 'a') as f:
-        f.write(f'Выбор {count}: {selected_choice}\n')
+    log_choice(win, lose)
 
     # Получаем новые варианты
     x = load_choices()
